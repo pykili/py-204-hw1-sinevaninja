cb16ffd75c4d9ca632e1d53cfc24164d354da93d
sinevaninja <72072001+sinevaninja@users.noreply.github.com>
2020-11-04 23:44:23 +0300

    Add files via upload
    я не понимаю как обозначить пустую строку, а в остальном все супер!

---
+++ task_4/номерок  4.py
@@ -0,0 +1,9 @@
+kittens=['ID', 'FORM', 'LEMMA', 'ЧТО_ТО_ЕЩЁ']
+a=""
+for i in range(10):
+    kittens[0],kittens[1],kittens[2],kittens[3]=input().split(" ")
+    if '#' in kittens[0]:
+        continue
+    print(kittens)
+        
+
